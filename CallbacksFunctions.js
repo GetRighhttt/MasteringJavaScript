6d29e25f619ca1847346fc6f2bb7ddf658bb4e8e
stefan <stefanbayne@gmail.com>
2022-07-29 14:59:17 -0400

    Callbacks explained

---
+++ CallbacksFunctions.js
@@ -0,0 +1,24 @@
+/**
+ * A callback is like a higher-order function in Kotlin.
+ * 
+ * Callbacks are functions that are passed as an argument to another function.
+ * 
+ * It's a function passed inside of a function to do more.
+ */
+
+// Here we wil demonstrate how to use a callback function
+const addNumbers = (a, b, callback) => {
+    console.log(a + b + callback(a, b)) // prints a + b then the callback
+}
+
+// we use arrow functions with callbacks to declare the call back function
+addNumbers(4, 6, (a, b) => {
+    return " The Callback Numbers are: " + a + " and " + b
+}) // this should print "10 The Callback Numbers are: 4 and 6"
+
+// we can also declare the callback in a variable
+const cb = (a, b) => {
+    return " This is the callback declared with numbers: " + a + " and " + b
+}
+// then when we make the function call, we pass in the const we created
+addNumbers(5, 10, cb) // prints "15 This is the callback declared with numbers: 5 and 10"
